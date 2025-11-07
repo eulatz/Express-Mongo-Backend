c867905273de8a108ccf641ef7edf87aacfda640
Laliu <laliosmp@gmail.com>
2025-11-07 14:40:03 +0100

    jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,47 @@
 import { validationResult } from "express-validator"
+import { User } from "../models/user.js"
+import jwt from "jsonwebtoken"
 
-export const register =  (req,res)=> {
-    console.log(req.body)
-    res.json({ok:true})
+export const register =  async(req,res)=> {
+        const {email,password} = req.body
+    try {
+        let user = await User.findOne({email})
+        if (user) throw {code: 11000}
+        
+        user = new User({email,password})
+        await user.save()
+
+        //JWT = Jason Web Token
+        return res.json({ok:true})
+    
+    } catch (error) {
+        console.log(error.code)
+        if (error.code === 11000) {
+            return res.status(400).json({error: "Ya existe este usuario"})
+        }
+        return res.status(500).json({error: "Error de servidor"})
+    }    
 }
 
-export const login =  (req,res)=> {
-    res.json({ok:true})
+export const login = async (req,res)=> {
+    try {
+        const {email,password} = req.body
+
+        let user = await User.findOne({email})
+        if (!user) return res.status(403).json({error: "No existe este usuario"})
+        
+        const respuestaPassword = await user.comparePassword(password)
+        if(!respuestaPassword){
+            return res.status(403).json({error: "Credencial Incorrecto"})
+        }
+// --- Generar JWT
+
+const token = jwt.sign({uid: user._id},process.env.JWT_SECRET)
+            return res.json({token})
+    
+    } catch (error) {
+        console.log(error.code)
+        return res.status(500).json({error: "Error de servidor"})
+    }    
 }
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-import {Schema, model} from "mongoose";
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -15,4 +16,23 @@ const userSchema = new Schema({
     },
 })
 
-export const User = model('user',userSchema)
+// --- Hashea Contraseña (Encripta) --- //
+userSchema.pre("save", async function(next) {
+    const user = this
+    if (!user.isModified("password")) return next()
+    try {
+       const salt = await bcryptjs.genSalt(10)
+       user.password = await bcryptjs.hash(user.password, salt)
+    next()
+    
+    } catch (error) {
+        console.error(error)
+        throw new Error("fallo hash de contraseña")
+    }
+
+})
+userSchema.methods.comparePassword = async function(clientPassword){
+    return await bcryptjs.compare(clientPassword, this.password)
+}
+
+export const User = mongoose.model('user',userSchema)
